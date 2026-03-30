32fd2cf27857d89b45a07a6bc3a366a221642c65
phelistus003 <phelistuskangu93@gmail.com>
2026-03-30 16:06:46 -0400

    fix: implement cart functions

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,37 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    return cart.reduce((total, item) => total + item.cost * item.quantity, 0);
  
   };
 
   const handleContinueShopping = (e) => {
+    onContinueShopping(e);
    
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      } else {
+        dispatch(removeItem(item.name));
+      }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.cost * item.quantity;
   };
 
   return (
